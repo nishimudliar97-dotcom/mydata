2e18fe9b99d9c90c1f50074941567593fe7cb409
nishimudliar97-dotcom <nishimudliar97@gmail.com>
2026-04-06 17:35:58 +0530

    Create a.py

---
+++ CT/a.py
@@ -0,0 +1,14 @@
+# Storm / Rain Ingress style trigger basis
+"storm": "Storm",
+"storm damage": "Storm",
+"windstorm": "Storm",
+"tempest": "Storm",
+
+"rain ingress": "Rain Ingress",
+"resultant rain ingress": "Resultant Rain Ingress",
+"storm resultant rain ingress": "Resultant Rain Ingress",
+"storm & resultant rain ingress": "Resultant Rain Ingress",
+"storm and resultant rain ingress": "Resultant Rain Ingress",
+"resultant rainfall ingress": "Resultant Rain Ingress",
+"rainwater ingress": "Rain Ingress",
+"water ingress": "Rain Ingress",
