24f9db21921d4e001a9043e2cfd584fdd9045cac
nishimudliar97-dotcom <nishimudliar97@gmail.com>
2026-05-05 15:41:52 +0530

    Update a.sql

--- a.sql
+++ a.sql
@@ -13,3 +13,21 @@ AS query
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'UNDERWRITING_RISK'
   AND TABLE_SCHEMA = 'REPORTING_COMMON';
+
+
+
+
+SELECT column_name,
+       COUNT(*) AS total_rows,
+       COUNT(value) AS filled_rows,
+       COUNT(value) * 100.0 / COUNT(*) AS fill_rate
+FROM du_prod.reporting_common.underwriting_risk
+UNPIVOT(value FOR column_name IN (
+    inwardlayerr_ntureason,
+    inwardsubmission_placingbasisname
+    -- add more columns
+))
+WHERE inwardrisksection_targetmarket IN ('Prop Ins Intl D&F', 'Prop Ins US D&F')
+  AND inwardsubmission_placingbasisname IN ('Insurance', 'Fac RI')
+  AND YEAR(inwardrisksection_inceptiondate) = 2025
+GROUP BY column_name;
